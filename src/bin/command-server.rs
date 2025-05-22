b38d3c218fdf918fbdc06b50ae39347b750d88ec
Shawn M Moore <code@sartak.org>
2025-05-22 11:34:16 -0400

    Add clap

--- src/bin/command-server.rs
+++ src/bin/command-server.rs
@@ -1,3 +1,26 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(long)]
+    start_command: String,
+
+    #[arg(long)]
+    stop_command: String,
+
+    #[arg(long)]
+    status_command: String,
+
+    #[arg(long)]
+    port: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let Args {
+        start_command,
+        stop_command,
+        status_command,
+        port,
+    } = Args::parse();
+    dbg!(start_command, stop_command, status_command, port);
 }
